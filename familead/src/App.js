4ac69c0ab8057c52170f0b362c44e816b8373831
Rohit Anantha <anantha.rohit.11@gmail.com>
2021-10-30 13:20:25 -0500

    firebase?

--- familead/src/App.js
+++ familead/src/App.js
@@ -1,6 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDhtzd3AtUln5CG_N3PXysa1bBcJMcwyrY",
+
+  authDomain: "familead-d41ef.firebaseapp.com",
+
+  projectId: "familead-d41ef",
+
+  storageBucket: "familead-d41ef.appspot.com",
+
+  messagingSenderId: "988772516971",
+
+  appId: "1:988772516971:web:5238ebb86a2260d1755d2e",
+
+  measurementId: "G-PCM6L739PB"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
@@ -21,5 +64,16 @@ function App() {
     </div>
   );
 }
+
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
 
 export default App;
