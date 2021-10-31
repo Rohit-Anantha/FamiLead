178585a85bfcdbb86058d0196dc2651742c39162
Rohit Anantha <anantha.rohit.11@gmail.com>
2021-10-31 09:30:27 -0500

    created a searchable maps api

--- familead/src/App.js
+++ familead/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var front = "https://www.google.com/maps/embed/v1/search?q=";
+  var back = "&key=AIzaSyAqtsSnyopgoCqC3XyE9H79maOdEgJfo3g";
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form >
+          <label>
+            What are you looking for near you?<br></br>
+            <h5>
+              Examples: Restaurants, Coffee Shops, Bars, etc.
+            </h5>
+            <input type="text" name="search" id="searchbox" />
+          </label>
+          <input type="submit" value="Submit" id="submitButton" />
+        </form>
+        <iframe width="600" height="450" loading="lazy" allowFullScreen src={front + urlParams.get('search') + back}></iframe>
       </header>
     </div>
   );
 }
+
 
 export default App;
